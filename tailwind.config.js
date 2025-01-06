0278c73bafbf21f55087578d4e85a91071cb7015
Thomas Hewitt <thomas.hewitt2201@gmail.com>
2025-01-06 08:56:29 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './templates/*.liquid',
+    './snippets/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
